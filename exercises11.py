aa23ccba8dc14e33c6b9e2ce636b35db1007af5f
OyuNet <oyunarda662@gmail.com>
2023-10-20 20:37:45 +0300

    while'lardan kurtulundu

--- exercises11.py
+++ exercises11.py
@@ -1,21 +1,8 @@
 givenTime = int(input("Enter seconds: "))
 
-storeTime = givenTime
-
-convDay = 0
-convHour = 0
-convMin = 0
-
-while (givenTime > 86400):
-    givenTime = givenTime-86400
-    convDay = convDay + 1
+convDay = givenTime // 86400
+convHour = (givenTime-convDay*86400) // 3600
+convMin = (givenTime-convDay*86400-convHour*3600) // 60
+convSec = (givenTime-convDay*86400-convHour*3600-convMin*60)
     
-while (givenTime > 3600):
-    givenTime = givenTime-3600
-    convHour = convHour + 1
-    
-while (givenTime > 60):
-    givenTime = givenTime-60
-    convMin = convMin + 1
-    
-print("{total} seconds = {day} day(s) {hour} hour(s) {min} minute(s) {sec} second(s)".format(total = storeTime, day = convDay, hour = convHour, min = convMin, sec = givenTime))
+print("{total} seconds = {day} day(s) {hour} hour(s) {min} minute(s) {sec} second(s)".format(total = givenTime, day = convDay, hour = convHour, min = convMin, sec = convSec))
